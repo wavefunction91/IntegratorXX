a5a1837bbfe753ae4bb29ae364c2e07dbf729338
David Williams-Young <dbwy@lbl.gov>
2023-07-07 17:36:34 -0700

    Missing headers

--- include/integratorxx/batch/spherical_micro_batcher.hpp
+++ include/integratorxx/batch/spherical_micro_batcher.hpp
@@ -7,6 +7,7 @@
 #include <iomanip>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 namespace IntegratorXX {
 
--- include/integratorxx/quadratures/gausscheby1.hpp
+++ include/integratorxx/quadratures/gausscheby1.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 #include <integratorxx/quadrature.hpp>
 
 namespace IntegratorXX {
