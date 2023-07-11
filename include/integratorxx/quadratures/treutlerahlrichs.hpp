5842dbb0ef8ab2ad08bd74d756b4633c65e64bb4
David Williams-Young <dbwy@lbl.gov>
2023-07-11 16:36:32 -0700

    Rename TreutlerAhlrichs{M4,}RadialTraits, add dox for M3 code path

--- include/integratorxx/quadratures/treutlerahlrichs.hpp
+++ include/integratorxx/quadratures/treutlerahlrichs.hpp
@@ -7,14 +7,14 @@
 namespace IntegratorXX {
 
 /**
- *  @brief Implementation of the Treutler-Ahlrichs M4 radial 
+ *  @brief Implementation of the Treutler-Ahlrichs M3+M4 radial 
  *  quadrature transformation rules.
  *
  *  Reference:
  *  J. Chem. Phys. 102, 346 (1995)
  *  DOI: https://doi.org/10.1063/1.469408
  */
-class TreutlerAhlrichsM4RadialTraits {
+class TreutlerAhlrichsRadialTraits {
 
   double R_;
   double alpha_;
@@ -24,13 +24,22 @@ public:
   inline static constexpr double a     = 1.0;
   inline static constexpr double ln_2  = 0.693147180559945309417232;
 
-  TreutlerAhlrichsM4RadialTraits(double R = 1.0, double alpha = 0.6) :
+  /**
+   *  Specify Treutler-Ahlrichs quadrature parameters
+   *
+   *  M3: Equation (18) of J. Chem. Phys. 102, 346 (1995)
+   *  M4: Equation (19) of J. Chem. Phys. 102, 346 (1995)
+   *
+   *  Default to M4 (alpha = 0.6). M3 resolved with alpha = 0.0
+   *
+   *  @param[in] R     Radial scaling factor
+   *  @param[in] alpha TA exponential factor
+   */
+  TreutlerAhlrichsRadialTraits(double R = 1.0, double alpha = 0.6) :
     R_(R), alpha_(alpha) { }
 
   /**
-   *  @brief Transformation rule for the TA M4 radial quadrature
-   *
-   *  Equation (19) of J. Chem. Phys. 102, 346 (1995)
+   *  @brief Transformation rule for the TA M3+M4 radial quadratures
    *  
    *  @param[in] x Point in (-1,1)
    *  @return    r = (a+x)^alpha * log((a+1)/(1-x)) / ln(2) 
@@ -44,7 +53,7 @@ public:
 
 
   /**
-   *  @brief Jacobian of the TA M4 radial transformation
+   *  @brief Jacobian of the TA M3+M4 radial transformations
    *
    *  @param[in] x Point in (-1,1)
    *  @returns   dr/dx (see `radial_transform`)
@@ -63,11 +72,11 @@ public:
  *  @brief Implementation of the Treutler-Ahlrichs M4 radial quadrature.
  *
  *  Taken as the convolution of the Gauss-Chebyshev (second kind) quadrature 
- *  with the TA M4 radial transformation. See TreutlerAhlrichsM4RadialTraits for
+ *  with the TA M4 radial transformation. See TreutlerAhlrichsRadialTraits for
  *  details.
  *
  *  Suitable for integrands which tend to zero as their argument tends 
- *  to 0 and inf. Tailored for radial integrands, i.e. r^2 * f(r), with 
+ f(r), with 
  *  lim_{r->inf} f(r) = 0.
  *
  *  Reference:
@@ -80,7 +89,7 @@ public:
 template <typename PointType, typename WeightType>
 using TreutlerAhlrichs = RadialTransformQuadrature<
   GaussChebyshev2<PointType, WeightType>,
-  TreutlerAhlrichsM4RadialTraits 
+  TreutlerAhlrichsRadialTraits 
 >;
 
 }
