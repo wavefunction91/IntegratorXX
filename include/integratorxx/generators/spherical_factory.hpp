031023c538ee4b3fa6819fadf1821d80d2ab79c9
David Williams-Young <dbwy@lbl.gov>
2023-11-01 16:32:15 -0700

    Added runtime generators for spherical quadratures and pruning schemes (migrated from GauXC)

---
+++ include/integratorxx/generators/spherical_factory.hpp
@@ -0,0 +1,157 @@
+#pragma once
+#include <memory>
+#include <vector>
+#include <array>
+#include <integratorxx/composite_quadratures/spherical_quadrature.hpp>
+#include <integratorxx/composite_quadratures/pruned_spherical_quadrature.hpp>
+
+namespace IntegratorXX {
+
+/// High-level specification of radial quadratures
+enum class RadialQuad : uint32_t {
+  Becke             = 0x0010, 
+  MurrayHandyLaming = 0x0020,
+  MuraKnowles       = 0x0030, 
+  TreutlerAhlrichs  = 0x0040
+};
+
+/// High-level specification of angular quadratures
+enum class AngularQuad : uint32_t {
+  AhrensBeylkin     = 0x0100,
+  Delley            = 0x0200,
+  LebedevLaikov     = 0x0300,
+  Womersley         = 0x0400
+};
+
+/// High-level specification of pruning schemes for spherical quadratures
+enum class PruningScheme {
+  Unpruned, /// Unpruned quadrature
+  Robust,   /// The "Robust" scheme of Psi4
+  Treutler  /// The Treutler-Aldrichs scheme
+};
+
+// TODO: Make these strong (non-convertible) types
+using RadialScale = double;
+using RadialSize  = size_t;
+using AngularSize = size_t;
+
+
+/// Generic specification of an unpruned spherical quadrature
+struct UnprunedSphericalGridSpecification {
+  RadialQuad  radial_quad;  ///< Radial quadrature specification
+  RadialSize  radial_size;  ///< Number of radial quadrature points
+  RadialScale radial_scale; ///< Radial scaling factor
+
+  AngularQuad angular_quad; /// Angular quadrature specification
+  AngularSize angular_size; /// Number of angular quadrature points
+};
+
+
+/// Speficiation of a pruned region of an spierical quadrature
+struct PruningRegion {
+  size_t idx_st; ///< Starting radial index for pruned region
+  size_t idx_en; ///< Ending radial index (exclusive) for the pruned region
+
+  AngularQuad angular_quad;  ///< Angular quadrature for the pruned region
+  AngularSize angular_size;  ///< Number of angular quadrature points in the pruned region
+
+  /// Check equality of `PruningRegion` instances
+  inline bool operator==(const PruningRegion& other) const noexcept {
+    return other.idx_st == idx_st and 
+           other.idx_en == idx_en and
+           other.angular_size == angular_size;
+  }
+};
+
+struct PrunedSphericalGridSpecification {
+  RadialQuad  radial_quad;  ///< Radial quadrature specification
+  RadialSize  radial_size;  ///< Number of radial quadrature points
+  RadialScale radial_scale; ///< Radial scaling factor
+
+  std::vector<PruningRegion> pruning_regions; ///< List of pruning regions over the radial quadrature
+};
+
+/// Generate a "Robust"-Psi4 Pruning specification from an 
+/// unpruned quadrature specification
+PrunedSphericalGridSpecification robust_psi4_pruning_scheme(
+  AngularQuad, UnprunedSphericalGridSpecification
+);
+
+/// Generate a Pruning specification according to the Treutler-Ahlrichs 
+/// scheme from an unpruned specification
+PrunedSphericalGridSpecification treutler_pruning_scheme(
+  AngularQuad, UnprunedSphericalGridSpecification
+);
+
+/// Generate a pruning specification from a specificed pruning scheme and 
+/// an unpruned grid specification
+PrunedSphericalGridSpecification create_pruned_spec(
+  PruningScheme, AngularQuad, UnprunedSphericalGridSpecification
+);
+
+
+
+/// Factory for spherical grids
+struct SphericalGridFactory {
+
+  template <typename RadialType, typename AngularType>
+  using unpruned_sphere_type = SphericalQuadrature< 
+      std::decay_t<RadialType>, std::decay_t<AngularType>
+    >;
+  template <typename RadialType, typename AngularType>
+  using pruned_sphere_type = PrunedSphericalQuadrature< 
+      std::decay_t<RadialType>, std::decay_t<AngularType>
+    >;
+
+  using spherical_grid_ptr = std::shared_ptr<
+    SphericalQuadratureBase<
+      std::vector<std::array<double,3>>,
+      std::vector<double>
+    >
+  >;
+
+  /**
+   *  @brief Generate an unpruned spherical grid given a suppled radial and
+   *         angular quadrature.
+   *
+   *  All arguments are passed with perfect forwarding
+   *
+   *  @tparam RadialType Type of the radial quadrature
+   *  @tparam AngularType Type of the angular quadrature
+   * 
+   *  @oaram     rq Radial quadrature from which to construct the spherical quadrature. 
+   *  @oaram     aq Angular quadrature from which to construct the spherical quadrature. 
+   *  @param[in] bsz Batch size for the grid generation.
+   */
+  template <typename RadialType, typename AngularType>
+  static spherical_grid_ptr generate_unpruned_grid( RadialType&& rq, 
+    AngularType&& aq ) {
+    using sphere_type = unpruned_sphere_type<RadialType,AngularType>;
+    return std::make_shared<sphere_type>( 
+      std::forward<RadialType>(rq), std::forward<AngularType>(aq) 
+    );
+  }
+
+  template <typename RadialType, typename RadialPartitionType>
+  static spherical_grid_ptr generate_pruned_grid( RadialType&& rq, 
+    RadialPartitionType&& rgp ) {
+    using angular_type = typename std::decay_t<RadialPartitionType>::angular_type;
+    using sphere_type = pruned_sphere_type<RadialType,angular_type>;
+    return std::make_shared<sphere_type>( 
+      std::forward<RadialType>(rq), std::forward<RadialPartitionType>(rgp)
+    );
+  }
+
+
+  static spherical_grid_ptr generate_unpruned_grid( RadialQuad, RadialSize, 
+    RadialScale, AngularQuad, AngularSize );
+  static spherical_grid_ptr generate_pruned_grid( RadialQuad, RadialSize, 
+    RadialScale, const std::vector<PruningRegion>&);
+
+
+  static spherical_grid_ptr generate_grid(UnprunedSphericalGridSpecification gs);
+  static spherical_grid_ptr generate_grid(PrunedSphericalGridSpecification gs); 
+
+};
+
+}
---
+++ src/spherical_factory.cxx
@@ -0,0 +1,370 @@
+#include <integratorxx/generators/spherical_factory.hpp>
+#include <integratorxx/quadratures/radial.hpp>
+#include <integratorxx/quadratures/s2.hpp>
+
+
+namespace IntegratorXX {
+
+using spherical_grid_ptr = SphericalGridFactory::spherical_grid_ptr;
+
+using bk_type  = Becke<double,double>;
+using mk_type  = MuraKnowles<double,double>;
+using mhl_type = MurrayHandyLaming<double,double>;
+using ta_type  = TreutlerAhlrichs<double,double>;
+
+using ah_type = AhrensBeylkin<double>;
+using de_type = Delley<double>;
+using ll_type = LebedevLaikov<double>;
+using wo_type = Womersley<double>;
+
+/************************/
+/**** Unpruned Grids ****/
+/************************/
+template <typename AngularQuadType>
+spherical_grid_ptr generate_unpruned_grid_impl(RadialQuad rq, RadialSize nrad,
+  RadialScale rscal, AngularQuadType&& ang_quad) {
+
+  switch( rq ) {
+
+    case RadialQuad::Becke:
+      return SphericalGridFactory::generate_unpruned_grid( bk_type(nrad, rscal), std::forward<AngularQuadType>(ang_quad) );
+
+    case RadialQuad::MuraKnowles:
+      return SphericalGridFactory::generate_unpruned_grid( mk_type(nrad, rscal), std::forward<AngularQuadType>(ang_quad) );
+
+    case RadialQuad::MurrayHandyLaming:
+      return SphericalGridFactory::generate_unpruned_grid( mhl_type(nrad, rscal), std::forward<AngularQuadType>(ang_quad) );
+
+    case RadialQuad::TreutlerAhlrichs:
+      return SphericalGridFactory::generate_unpruned_grid( ta_type(nrad, rscal), std::forward<AngularQuadType>(ang_quad) );
+
+    default:
+      throw std::runtime_error("Unsupported Radial Quadrature");
+      abort();
+
+  }
+
+
+}
+
+spherical_grid_ptr SphericalGridFactory::generate_unpruned_grid( RadialQuad rq, 
+  RadialSize nrad, RadialScale rscal, AngularQuad aq, AngularSize nang) {
+
+  switch(aq) {
+    case AngularQuad::AhrensBeylkin:
+      return generate_unpruned_grid_impl(rq, nrad, rscal, ah_type(nang));
+    case AngularQuad::Delley:
+      return generate_unpruned_grid_impl(rq, nrad, rscal, de_type(nang));
+    case AngularQuad::LebedevLaikov:
+      return generate_unpruned_grid_impl(rq, nrad, rscal, ll_type(nang));
+    case AngularQuad::Womersley:
+      return generate_unpruned_grid_impl(rq, nrad, rscal, wo_type(nang));
+    default:
+      throw std::runtime_error("Unsupported Angular Quadrature");
+      abort();
+  }
+}
+
+spherical_grid_ptr SphericalGridFactory::generate_grid( 
+  UnprunedSphericalGridSpecification gs ) {
+  return generate_unpruned_grid(gs.radial_quad, gs.radial_size, gs.radial_scale,
+    gs.angular_quad, gs.angular_size);
+}
+
+
+
+
+/**********************/
+/**** Pruned Grids ****/
+/**********************/
+
+template <typename AngularQuadType, typename RadialQuadType>
+auto make_pruned_grid_impl(const RadialQuadType& rq, 
+  const std::vector<PruningRegion>& pruning_regions) {
+
+  RadialGridPartition<AngularQuadType> rgp;
+  for( auto& region : pruning_regions ) {
+    rgp.add_quad( rq, region.idx_st, AngularQuadType(region.angular_size) );
+  }
+  rgp.finalize(rq);
+
+  return SphericalGridFactory::generate_pruned_grid(rq, std::move(rgp));
+
+}
+
+template <typename RadialQuadType>
+auto make_pruned_grid(const RadialQuadType& rq, 
+  const std::vector<PruningRegion>& pruning_regions) {
+
+  if(pruning_regions.size() == 0)
+    throw std::runtime_error("No Pruning Regions");
+
+  auto angular_quad = pruning_regions[0].angular_quad;
+  for(auto r : pruning_regions) {
+    if(r.angular_quad != angular_quad)
+      throw std::runtime_error("Mixed Angular Pruning Not Supported");
+  }
+
+  switch(angular_quad) {
+    case AngularQuad::AhrensBeylkin:
+      return make_pruned_grid_impl<ah_type>(rq, pruning_regions);
+    case AngularQuad::Delley:
+      return make_pruned_grid_impl<de_type>(rq, pruning_regions);
+    case AngularQuad::LebedevLaikov:
+      return make_pruned_grid_impl<ll_type>(rq, pruning_regions);
+    case AngularQuad::Womersley:
+      return make_pruned_grid_impl<wo_type>(rq, pruning_regions);
+    default:
+      throw std::runtime_error("Unsupported Angular Quadrature");
+      abort();
+  }
+  
+
+}
+
+spherical_grid_ptr SphericalGridFactory::generate_pruned_grid( 
+  RadialQuad rq, RadialSize nrad, RadialScale rscal, 
+  const std::vector<PruningRegion>& pruning_regions) { 
+
+  switch( rq ) {
+
+    case RadialQuad::Becke:
+      return make_pruned_grid( bk_type(nrad, rscal), pruning_regions );
+    case RadialQuad::MuraKnowles:
+      return make_pruned_grid( mk_type(nrad, rscal), pruning_regions );
+    case RadialQuad::MurrayHandyLaming:
+      return make_pruned_grid( mhl_type(nrad, rscal), pruning_regions );
+    case RadialQuad::TreutlerAhlrichs:
+      return make_pruned_grid( ta_type(nrad, rscal), pruning_regions );
+
+    default:
+      throw std::runtime_error("Unsupported Radial Quadrature");
+      abort();
+
+  }
+
+}
+
+spherical_grid_ptr SphericalGridFactory::generate_grid( 
+  PrunedSphericalGridSpecification gs ) {
+  return generate_pruned_grid( gs.radial_quad, gs.radial_size, 
+    gs.radial_scale, gs.pruning_regions );
+}
+
+
+
+
+/***************************
+ * Default Pruning Schemes *
+ ***************************/
+
+template <typename AngularQuad>
+struct s2_quad_traits;
+
+template <>
+struct s2_quad_traits<ll_type> {
+  static auto npts_by_algebraic_order(size_t order) {
+    return detail::lebedev::npts_by_algebraic_order(order);
+  }
+  static auto next_algebraic_order(size_t order) {
+    return detail::lebedev::next_algebraic_order(order);
+  }
+  static auto algebraic_order_by_npts(size_t order) {
+    return detail::lebedev::algebraic_order_by_npts(order);
+  }
+};
+
+template <>
+struct s2_quad_traits<ah_type> {
+  static auto npts_by_algebraic_order(size_t order) {
+    return detail::ahrensbeylkin::npts_by_algebraic_order(order);
+  }
+  static auto next_algebraic_order(size_t order) {
+    return detail::ahrensbeylkin::next_algebraic_order(order);
+  }
+  static auto algebraic_order_by_npts(size_t order) {
+    return detail::ahrensbeylkin::algebraic_order_by_npts(order);
+  }
+};
+
+template <>
+struct s2_quad_traits<de_type> {
+  static auto npts_by_algebraic_order(size_t order) {
+    return detail::delley::npts_by_algebraic_order(order);
+  }
+  static auto next_algebraic_order(size_t order) {
+    return detail::delley::next_algebraic_order(order);
+  }
+  static auto algebraic_order_by_npts(size_t order) {
+    return detail::delley::algebraic_order_by_npts(order);
+  }
+};
+
+template <>
+struct s2_quad_traits<wo_type> {
+  static auto npts_by_algebraic_order(size_t order) {
+    return detail::womersley::npts_by_algebraic_order(order);
+  }
+  static auto next_algebraic_order(size_t order) {
+    return detail::womersley::next_algebraic_order(order);
+  }
+  static auto algebraic_order_by_npts(size_t order) {
+    return detail::womersley::algebraic_order_by_npts(order);
+  }
+};
+
+
+
+
+
+/*** Psi4 Robust ***/
+
+
+template <typename AngularQuad>
+auto get_robust_low_med_sizes(AngularSize asz) {
+  using traits = s2_quad_traits<AngularQuad>;
+  const auto base_order = traits::algebraic_order_by_npts(asz);
+  if( base_order < 0 ) throw std::runtime_error("Invalid Base Grid");
+
+  const auto med_order = 
+    traits::next_algebraic_order(base_order > 6 ? base_order-6 : base_order);
+  const auto low_order = 7;
+  
+  AngularSize med_sz(traits::npts_by_algebraic_order(med_order));
+  AngularSize low_sz(traits::npts_by_algebraic_order(low_order));
+
+  return std::make_pair(low_sz, med_sz);
+}
+
+
+PrunedSphericalGridSpecification robust_psi4_pruning_scheme_impl(
+  size_t low_sz, size_t med_sz, AngularQuad angular_quad, 
+  UnprunedSphericalGridSpecification unp ) {
+
+  const size_t rsz = unp.radial_size;
+  const size_t r_div_4 = rsz / 4ul + 1ul;
+  const size_t r_div_2 = rsz / 2ul + 1ul;
+  std::vector<PruningRegion> pruning_regions = {
+    {0ul,     r_div_4, angular_quad, low_sz},
+    {r_div_4, r_div_2, angular_quad, med_sz},
+    {r_div_2,     rsz, angular_quad, unp.angular_size}
+  };
+
+  return PrunedSphericalGridSpecification{
+    unp.radial_quad, unp.radial_size, unp.radial_scale, pruning_regions
+  };
+  
+}
+
+PrunedSphericalGridSpecification robust_psi4_pruning_scheme(
+  AngularQuad angular_quad, UnprunedSphericalGridSpecification unp ) {
+
+  size_t low_sz, med_sz;
+  const auto asz = unp.angular_size;
+  switch(angular_quad) {
+    case AngularQuad::AhrensBeylkin:
+      std::tie(low_sz, med_sz) = get_robust_low_med_sizes<ah_type>(asz);
+      break;
+    case AngularQuad::Delley:
+      std::tie(low_sz, med_sz) = get_robust_low_med_sizes<de_type>(asz);
+      break;
+    case AngularQuad::LebedevLaikov:
+      std::tie(low_sz, med_sz) = get_robust_low_med_sizes<ll_type>(asz);
+      break;
+    case AngularQuad::Womersley:
+      std::tie(low_sz, med_sz) = get_robust_low_med_sizes<wo_type>(asz);
+      break;
+    default:
+      throw std::runtime_error("Unsupported Angular Quadrature");
+      abort();
+  }
+
+  return robust_psi4_pruning_scheme_impl(low_sz, med_sz, angular_quad, unp);
+}
+
+
+
+/*** Treutler-Ahlrichs Pruning ***/
+
+
+template <typename AngularQuad>
+auto get_treutler_low_med_sizes() {
+  using traits = s2_quad_traits<AngularQuad>;
+  AngularSize med_sz(traits::npts_by_algebraic_order(traits::next_algebraic_order(11)));
+  AngularSize low_sz(traits::npts_by_algebraic_order(traits::next_algebraic_order(7 )));
+
+  return std::make_pair(low_sz, med_sz);
+}
+
+PrunedSphericalGridSpecification treutler_pruning_scheme_impl(
+  size_t low_sz, size_t med_sz, AngularQuad angular_quad, 
+  UnprunedSphericalGridSpecification unp ) {
+
+  const size_t rsz = unp.radial_size;
+  const size_t r_div_3 = rsz / 3ul + 1ul;
+  const size_t r_div_2 = rsz / 2ul + 1ul;
+  std::vector<PruningRegion> pruning_regions = {
+    {0ul,     r_div_3, angular_quad, low_sz},
+    {r_div_3, r_div_2, angular_quad, med_sz},
+    {r_div_2, rsz,     angular_quad, unp.angular_size}
+  };
+
+  return PrunedSphericalGridSpecification{
+    unp.radial_quad, unp.radial_size, unp.radial_scale, pruning_regions
+  };
+  
+}
+
+
+
+PrunedSphericalGridSpecification treutler_pruning_scheme(
+  AngularQuad angular_quad, UnprunedSphericalGridSpecification unp ) {
+
+  size_t low_sz, med_sz;
+  switch(angular_quad) {
+    case AngularQuad::AhrensBeylkin:
+      std::tie(low_sz, med_sz) = get_treutler_low_med_sizes<ah_type>();
+      break;
+    case AngularQuad::Delley:
+      std::tie(low_sz, med_sz) = get_treutler_low_med_sizes<de_type>();
+      break;
+    case AngularQuad::LebedevLaikov:
+      std::tie(low_sz, med_sz) = get_treutler_low_med_sizes<ll_type>();
+      break;
+    case AngularQuad::Womersley:
+      std::tie(low_sz, med_sz) = get_treutler_low_med_sizes<wo_type>();
+      break;
+    default:
+      throw std::runtime_error("Unsupported Angular Quadrature");
+      abort();
+  }
+
+  return treutler_pruning_scheme_impl(low_sz, med_sz, angular_quad, unp);
+}
+
+
+
+PrunedSphericalGridSpecification create_pruned_spec(
+  PruningScheme scheme, AngularQuad aq, UnprunedSphericalGridSpecification unp
+) {
+
+  switch(scheme) {
+    case PruningScheme::Robust:
+      return robust_psi4_pruning_scheme(aq, unp);
+    case PruningScheme::Treutler:
+      return treutler_pruning_scheme(aq, unp);
+    
+    // Default to Unpruned Grid
+    case PruningScheme::Unpruned:
+    default:
+      std::vector<PruningRegion> pruning_regions = {
+        {0ul, (size_t)unp.radial_size, aq, unp.angular_size}
+      };
+      return PrunedSphericalGridSpecification{
+        unp.radial_quad, unp.radial_size, unp.radial_scale, pruning_regions
+      };
+  }
+
+}
+
+}
